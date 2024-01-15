ecec2766c011bec64976e43969e8fb3393d041e0
TheDeadlyBots / <94062864+piroXpower@users.noreply.github.com>
2024-01-15 12:15:20 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,6 +1,6 @@
 
-from pyrogram import Client, filters
-from pyrogram.types import StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client, filters, StopPropagation
+from pyrogram.types import  InlineKeyboardButton, InlineKeyboardMarkup
 
 
 @Client.on_message(filters.command(["start"]), group=-2)
