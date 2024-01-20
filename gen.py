caa897022726ac5f05e0c6da8079b2dce224a57a
TheDeadlyBots / <94062864+piroXpower@users.noreply.github.com>
2024-01-20 20:59:12 +0530

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 
-api_id = "your_api_id"
-api_hash = "your_api_hash"
+api_id = "21364355"
+api_hash = "72f11aec1dd3e5764554d477341a3d0b"
 
 with Client("your_session_name", api_id, api_hash) as app:
     print(app.export_session_string())
